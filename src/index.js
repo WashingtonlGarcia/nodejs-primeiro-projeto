2d9d869e9c7ab1cfe4f985929520a6ed9a19d41c
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2021-03-11 22:54:53 -0300

    foi criando um middleware para validar se existe um customer.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const {v4: uuidv4} = require('uuid')
+const { request } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const customers = []
+const customers = [];
 app.use(express.json());
 
-app.post('/account', (req, res) => {
-  const {cpf, name} = req.body;
-  if (customers.some((customer) => customer.cpf === cpf)) {
-    return res.status(400).json({error: 'Customer already Exists!'})
-  }
+const verifyIfExistAccountCPF = (req, res, next) => {
+  const { cpf } = req.headers;
+  const costomer = customers.find((costomer) => costomer.cpf === cpf);
+  if (!costomer) return res.status(400).json({ error: "Customer not found" });
+  request.customer = costomer;
+  return next();
+};
 
-  const account = {cpf, name, id: uuidv4(), statement: []}
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  if (customers.some((customer) => customer.cpf === cpf)) {
+    return res.status(400).json({ error: "Customer already Exists!" });
+  }
+  const account = { cpf, name, id: uuidv4(), statement: [] };
   customers.push(account);
-  console.log(account)
-  console.log(customers)
   return res.status(201).json(account);
 });
 
+app.get("/statement",verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
+  return res.json(customer);
+});
+
 app.listen(3003, () => {
-  console.log('Started backend in port 3334!!')
+  console.log("Started backend in port 3334!!");
 });
